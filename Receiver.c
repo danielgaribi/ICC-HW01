3877f1df93249850b5b54aea5b823d5e2dbbb38a
danielgaribi <danielgaribi@mail.tau.ac.il>
2022-03-09 16:56:27 +0200

    split to frames

--- Receiver.c
+++ Receiver.c
@@ -8,8 +8,17 @@
 
 #pragma comment (lib, "Ws2_32.lib")
 
-#define ALLOC_BLOCK 1024
-#define MAX_STR_LEN 1024
+#define ALLOC_BLOCK             1024
+#define MAX_STR_LEN             1024
+
+#define FRAME_NOF_DATA_BITS     26
+#define FRAME_NOF_BITS          31
+#define NOF_FRAMS_IN_PACKET     8
+#define NOF_BITS_IN_BYTE        8
+#define DATA_BUFFER_LENFTH      CONVERT_BIT_TO_BYTES(FRAME_NOF_DATA_BITS * NOF_FRAMS_IN_PACKET)
+#define ENC_BUFFER_LENFTH       CONVERT_BIT_TO_BYTES(FRAME_NOF_BITS * NOF_FRAMS_IN_PACKET)
+
+#define CONVERT_BIT_TO_BYTES(_nof_bits)    ((_nof_bits) / NOF_BITS_IN_BYTE)
 
 #define ASSERT(_con, _msg)    if (!(_con)) {\
                                     perror("Error!\n");\
@@ -18,60 +27,96 @@
                                     exit(1);\
                                 }
 
-char* get_buffer(int socket_fd, uint32_t *buffer_length) {
+int get_buffer(int socket_fd, char* enc_buffer, uint32_t buffer_length) {
     int total_nof_bytes_rec = 0;
     int nof_bytes_rec;
-    char* buffer, *current_buffer_pointer;
-    ASSERT((buffer = (char *) calloc(ALLOC_BLOCK, sizeof(char))) != NULL, "calloc failed");
-    *buffer_length = ALLOC_BLOCK;
-    current_buffer_pointer = buffer;
+    char* current_buffer_pointer = enc_buffer;
 
     do {
-        ASSERT((nof_bytes_rec = recv(socket_fd, current_buffer_pointer, *buffer_length - total_nof_bytes_rec, 0)) >= 0, "recv failed");
-        printf("c: %d\n", current_buffer_pointer[0]);
+        ASSERT((nof_bytes_rec = recv(socket_fd, current_buffer_pointer, buffer_length - total_nof_bytes_rec, 0)) >= 0, "recv failed");
         total_nof_bytes_rec += nof_bytes_rec;
-        if (*buffer_length == total_nof_bytes_rec) {
-            *buffer_length = ((*buffer_length / ALLOC_BLOCK) + 1) * ALLOC_BLOCK;
-            ASSERT((buffer = (char *) realloc(buffer, *buffer_length * sizeof(char))) != NULL, "realloc failed");
+        if (buffer_length == total_nof_bytes_rec) {
+            break;
         }
         current_buffer_pointer += total_nof_bytes_rec;
 
     } while(nof_bytes_rec > 0);
 
-    *buffer_length = total_nof_bytes_rec;
-    ASSERT((buffer = (char *) realloc(buffer, *buffer_length * sizeof(char))) != NULL, "realloc failed");
+    return total_nof_bytes_rec;
 
-    printf("c0: %c\n", buffer[0]);
-    return buffer;
+    // *buffer_length = total_nof_bytes_rec;
+    // ASSERT((buffer = (char *) realloc(buffer, *buffer_length * sizeof(char))) != NULL, "realloc failed");
+
+    // for(int i = 0; i < *buffer_length; i++){
+    //     printf("%d: '%u'\n", i, buffer[i]);
+    // }
 }
 
-int get_file_size(FILE *file_fd) {
-    int file_length;
-    fseek(file_fd, 0L, SEEK_END);
-    file_length = ftell(file_fd);
-    rewind(file_fd);
-    return file_length;
+uint32_t write_file(FILE *file_fd, uint32_t buffer_length, char *buffer) {
+    uint32_t nof_write_bytes = 0;
+    ASSERT((nof_write_bytes = fwrite(buffer, 1, buffer_length ,file_fd)) == buffer_length, "fwrite failed");
+    return nof_write_bytes;
 }
 
-void write_file(FILE *file_fd, int file_length, char *buffer) {
-    ASSERT(fwrite(buffer, 1, file_length ,file_fd) == file_length, "fwrite failed");
+void convert_bit_array_to_buffer(char *buffer, int buffer_length, short *bits_array, int bits_array_length) {
+    int i,j, index = 0;
+    char c;
+
+    for(i = 0; i < buffer_length; i++) {
+        c = 0;
+        for(j = 0; j < sizeof(short); j++) {
+            c |= bits_array[index];
+            c <= 1;
+            index++;
+        }
+        buffer[i] = c;
+    }
 }
 
-uint32_t dec_file(char *buffer, int buffer_length, char *enc_buffer, int enc_buffer_length) {
-    uint32_t nof_corrected_bytes = 0;
-    strcpy(buffer, enc_buffer);
+int dec_file(char *buffer, int buffer_length, char *enc_buffer, int enc_buffer_length) {
+    short data_bits_array[FRAME_NOF_DATA_BITS * NOF_FRAMS_IN_PACKET] = { 0 };
+    short enc_bits_array[FRAME_NOF_BITS] = { 0 };
+    short *sub_data_bits_array = data_bits_array;
+    short mask;
+    int i, j, index = 0;
+    int nof_corrected_bytes = 0;
+    for(i = 0; i < enc_buffer_length; i++) {
+        mask = 1 << (sizeof(short) - 1);
+        for(j = 0; j < sizeof(short); j++) {
+            enc_bits_array[index] = buffer[i] & mask;
+            index++;
+            if (index == FRAME_NOF_BITS) {
+                /* hamming!!! */
+                nof_corrected_bytes += 0;
+                sub_data_bits_array += FRAME_NOF_DATA_BITS;
+                index = 0;
+            }
+            mask >>= 1;
+        }
+    }
+    convert_bit_array_to_buffer(buffer, buffer_length, data_bits_array, FRAME_NOF_DATA_BITS * NOF_FRAMS_IN_PACKET);
     return nof_corrected_bytes;
 }
+
+// uint32_t dec_file(char *buffer, int buffer_length, char *enc_buffer, int enc_buffer_length) {
+//     uint32_t nof_corrected_bytes = 0;
+//     for(int i = 0; i < enc_buffer_length; i++){
+//         buffer[i] = enc_buffer[i];
+//     }
+//     return nof_corrected_bytes;
+// }
 
 int main(int argc, char* argv[]) {
     char *server_ip, file_path[MAX_STR_LEN];
     int server_port;
     int socket_fd;
     FILE *file_fd;
-    char *buffer, *enc_buffer;
-    uint32_t buffer_length, enc_buffer_length, nof_corrected_bytes;
+    uint32_t data_buffer_length, enc_buffer_length, nof_corrected_bytes, nof_write_bytes, recieved_bytes;
     struct sockaddr_in serv_addr;
     WSADATA wsaData;
+
+    char data_buffer[DATA_BUFFER_LENFTH] = { 0 };
+    char enc_buffer[ENC_BUFFER_LENFTH] = { 0 };
 
     ASSERT(WSAStartup(MAKEWORD(2,2), &wsaData) == NO_ERROR, "WSAStartup failed");
 
@@ -99,24 +144,36 @@ int main(int argc, char* argv[]) {
             break;
         }
 
-        printf("1\n");
+        ASSERT((file_fd = fopen(file_path, "wb")) >= 0, "open failed");
+
+        recieved_bytes = 0;
+        nof_corrected_bytes += 0;
+        nof_write_bytes += 0;
+        while (1) {
+            recieved_bytes += get_buffer(socket_fd, enc_buffer, enc_buffer_length);
+            if (recieved_bytes == 0) {
+                break;
+            }
+            nof_corrected_bytes += dec_file(data_buffer, data_buffer_length, enc_buffer, enc_buffer_length);
+            nof_write_bytes += write_file(file_fd, data_buffer_length, data_buffer);
+        } 
+        
 
         /* Get decrypted buffer */
-        enc_buffer = get_buffer(socket_fd, &enc_buffer_length);
-        printf("buf: %s\n", enc_buffer);
-        printf("received: %u bytes\n", enc_buffer_length);
-        printf("2\n");
+        // enc_buffer = get_buffer(socket_fd, &enc_buffer_length);
+        // printf("buf: %s\n", enc_buffer);
+        // printf("received: %u bytes\n", recieved_bytes);
 
-        buffer_length = enc_buffer_length;
-        ASSERT((buffer = (char *) calloc(buffer_length, sizeof(char))) != NULL, "calloc failed");
+        // buffer_length = enc_buffer_length;
+        // ASSERT((buffer = (char *) calloc(buffer_length, sizeof(char))) != NULL, "calloc failed");
 
-        nof_corrected_bytes = dec_file(buffer, buffer_length, enc_buffer, enc_buffer_length);
-        free(enc_buffer);
+        // nof_corrected_bytes = dec_file(buffer, buffer_length, enc_buffer, enc_buffer_length);
+        // free(enc_buffer);
 
-        ASSERT((file_fd = fopen(file_path, "w+")) >= 0, "open failed");
-        write_file(file_fd, buffer_length, buffer);
-        free(buffer);
-        printf("wrote: %u bytes\n", buffer_length);
+        // ASSERT((file_fd = fopen(file_path, "wb")) >= 0, "open failed");
+        // write_file(file_fd, buffer_length, buffer);
+        // free(buffer);
+        printf("wrote: %u bytes\n", nof_write_bytes);
         printf("corrected %u errors\n", nof_corrected_bytes);
         
         fclose(file_fd);
--- Sender.c
+++ Sender.c
@@ -11,6 +11,15 @@
 
 #define MAX_STR_LEN 1024
 
+#define FRAME_NOF_DATA_BITS     26
+#define FRAME_NOF_BITS          31
+#define NOF_FRAMS_IN_PACKET     8
+#define NOF_BITS_IN_BYTE        8
+#define DATA_BUFFER_LENFTH      CONVERT_BIT_TO_BYTES(FRAME_NOF_DATA_BITS * NOF_FRAMS_IN_PACKET)
+#define ENC_BUFFER_LENFTH       CONVERT_BIT_TO_BYTES(FRAME_NOF_BITS * NOF_FRAMS_IN_PACKET)
+
+#define CONVERT_BIT_TO_BYTES(_nof_bits)    ((_nof_bits) / NOF_BITS_IN_BYTE)
+
 #define ASSERT(_con, _msg)    if (!(_con)) {\
                                     perror("Error!\n");\
                                     printf("%d\n", WSAGetLastError());\
@@ -21,6 +30,10 @@
 void send_buffer(int socket_fd, char* buffer, uint32_t buffer_length) {
     int nof_bytes = buffer_length;
     int sent_bytes;
+
+    for(int i = 0; i < buffer_length; i++){
+        printf("%d: '%u'\n", i, buffer[i]);
+    }
 
     do {
         ASSERT((sent_bytes = send(socket_fd, buffer, nof_bytes, 0)) >= 0, "send failed");
@@ -39,19 +52,71 @@ int get_file_size(FILE *file_fd) {
     return file_length;
 }
 
-void read_file(FILE *file_fd, int file_length, char *buffer) {
-    char ch;
-    int i = 0;
-    while ((ch = fgetc(file_fd)) != EOF) {
-        buffer[i] = ch;
-        i++;
+int read_frame_from_file(FILE *file_fd, int file_length, char *buffer) {
+    int nof_read_bytes = 0;
+    int nof_remain_bytes = CONVERT_BIT_TO_BYTES(FRAME_NOF_DATA_BITS * NOF_FRAMS_IN_PACKET);
+    int total_nof_read_bytes = 0;
+    char *working_buffer = buffer;
+
+
+    // while ((nof_read_bytes = read(file_fd, buffer, 1000)) != nof_remain_bytes) {
+    while ((nof_read_bytes = fread(working_buffer, sizeof(char), nof_remain_bytes, file_fd)) != nof_remain_bytes) {
+        nof_remain_bytes -= nof_read_bytes;
+        working_buffer += nof_read_bytes;
+        total_nof_read_bytes += nof_read_bytes;
     }
-    printf("i=%d\n", i);
-    ASSERT(i == file_length, "Read Failed");
+    total_nof_read_bytes += nof_read_bytes;
+    return total_nof_read_bytes;
+    // printf("nof: %d\n", nof_remain_bytes);
+    // for(int i = 0; i < file_length; i++){
+    //     printf("%d: '%u'\n", i, buffer[i]);
+    // }
+
+    // char ch;
+    // int i = 0;
+
+    // while ((ch = fgetc(file_fd)) != EOF) {
+    //     buffer[i] = ch;
+    //     i++;
+    // }
+    // printf("i=%d\n", i);
+    // ASSERT(i == file_length, "Read Failed");
+}
+
+void convert_bit_array_to_buffer(char *enc_buffer, int enc_buffer_length, short *bits_array, int bits_array_length) {
+    int i,j, index = 0;
+    char c;
+
+    for(i = 0; i < enc_buffer_length; i++) {
+        c = 0;
+        for(j = 0; j < sizeof(short); j++) {
+            c |= bits_array[index];
+            c <= 1;
+            index++;
+        }
+        enc_buffer[i] = c;
+    }
 }
 
 void enc_file(char *buffer, int buffer_length, char *enc_buffer, int enc_buffer_length) {
-    strcpy(enc_buffer, buffer);
+    short bits_array[FRAME_NOF_BITS * NOF_FRAMS_IN_PACKET] = { 0 };
+    short *sub_bits_array = bits_array;
+    short mask;
+    int i, j, index = 0;
+    for(i = 0; i < buffer_length; i++) {
+        mask = 1 << (sizeof(short) - 1);
+        for(j = 0; j < sizeof(short); j++) {
+            sub_bits_array[index] = buffer[i] & mask;
+            index++;
+            if (index == FRAME_NOF_DATA_BITS) {
+                /* hamming!!! */
+                sub_bits_array += FRAME_NOF_BITS;
+                index = 0;
+            }
+            mask >>= 1;
+        }
+    }
+    convert_bit_array_to_buffer(enc_buffer, enc_buffer_length, bits_array, FRAME_NOF_BITS * NOF_FRAMS_IN_PACKET);
 }
 
 int main(int argc, char* argv[]) {
@@ -59,10 +124,12 @@ int main(int argc, char* argv[]) {
     int server_port;
     int socket_fd;
     FILE *file_fd;
-    char *buffer, *enc_buffer;
-    uint32_t file_length, enc_buffer_length;
+    uint32_t file_length, data_buffer_length, enc_buffer_length, read_bytes, sent_bytes;
     struct sockaddr_in serv_addr;
     WSADATA wsaData;
+
+    char data_buffer[DATA_BUFFER_LENFTH] = { 0 };
+    char enc_buffer[ENC_BUFFER_LENFTH] = { 0 };
 
     ASSERT(WSAStartup(MAKEWORD(2,2), &wsaData) == NO_ERROR, "WSAStartup failed");
 
@@ -91,27 +158,38 @@ int main(int argc, char* argv[]) {
             break;
         }
         
-        ASSERT((file_fd = fopen(file_path, "r")) >= 0, "open failed");
+        ASSERT((file_fd = fopen(file_path, "rb")) >= 0, "open failed");
 
         /* Get and print file length */
-        // file_length = get_file_size(file_fd);
-        file_length = 80;
+        file_length = get_file_size(file_fd);
+        // file_length = 80;
         printf("file length: %u bytes\n", file_length);
 
+        read_bytes = 0;
+        sent_bytes = 0;
         /* Read file and send */
-        ASSERT((buffer = (char *) calloc(file_length, sizeof(char))) != NULL, "calloc failed");
-        read_file(file_fd, file_length, buffer);
-        printf("buf: %s\n", buffer);
+        while(file_length != read_bytes) {
+            read_bytes += read_frame_from_file(file_fd, file_length, data_buffer);
+            enc_file(data_buffer, data_buffer_length, enc_buffer, enc_buffer_length);
+            send_buffer(socket_fd, enc_buffer, enc_buffer_length);
+            sent_bytes += enc_buffer_length;
+        }
 
-        enc_buffer_length = file_length;
-        ASSERT((enc_buffer = (char *) calloc(enc_buffer_length, sizeof(char))) != NULL, "calloc failed");
+        // /* Read file and send */
+        // read_file(file_fd, file_length, buffer);
 
-        enc_file(buffer, file_length, enc_buffer, enc_buffer_length);
-        free(buffer);
-        send_buffer(socket_fd, enc_buffer, enc_buffer_length);
-        free(enc_buffer);
+        // enc_buffer_length = file_length;
+        // ASSERT((enc_buffer = (char *) calloc(enc_buffer_length, sizeof(char))) != NULL, "calloc failed");
 
-        printf("sent: %u bytes\n", file_length);
+        // enc_file(buffer, file_length, enc_buffer, enc_buffer_length);
+        // for(int i = 0; i < enc_buffer_length; i++){
+        //     printf("%d: '%u'\n", i, enc_buffer[i]);
+        // }
+        // free(buffer);
+        // send_buffer(socket_fd, enc_buffer, enc_buffer_length);
+        // free(enc_buffer);
+
+        printf("sent: %u bytes\n", sent_bytes);
 
         fclose(file_fd);
         closesocket(socket_fd);
